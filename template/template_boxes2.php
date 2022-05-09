3ce82eb075c8647949efd364c96daa729c882503
lolaPandaERP <86048238+lolaPandaERP@users.noreply.github.com>
2022-05-09 10:08:46 +0200

    update templates for all pages

--- template/template_boxes2.php
+++ template/template_boxes2.php
@@ -1,80 +1,63 @@
 <head>
-        <meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" type="text/css" href="tab.css">
-		<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/css/bootstrap-extended.min.css">
-		<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/fonts/simple-line-icons/style.min.css">
-		<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/css/bootstrap.min.css">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-	</head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="tab.css">
+	<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/css/bootstrap-extended.min.css">
+	<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/fonts/simple-line-icons/style.min.css">
+	<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+</head>
 
 <body>
 
+	<!-- page content -->
+	<section class="page-content">
 
-
-
-<!-- page content -->
-<section class="page-content">
 		<!-- grid-based layout -->
-			<div class="grid-container-4">
+		<div class="grid-container-4">
 
 			<!-- bloc 1 -->
-				<div class="grid-1">
-					<div class="card bg-c-blue order-card">
-						<!-- Corps de la carte -->
-						<div class="card-body">
-							<div class="card-block">
-								<h4 class="text-center">
-									<?php print $titleItem1 ?> <!-- Titre de la boxe -->
-								</h4>
-								<h1 class="text-center">
-											<?= print $dataItem1?> €
-									</h1>
-									<hr>
-									<div class="element">
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-										<?php print $info1 ?> :
-										</br><?php print $dataInfo1?>
-										<hr class="vertical">
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-										<p><?php print $info2 ?>: </hr>
-										<p id="progress"><?php print $dataInfo2?>
-									</div>
-								</div>
+				<div class="card bg-c-blue order-card">
+					<!-- Corps de la carte -->
+					<div class="card-body">
+						<!-- <div class="card-block"> -->
+						<h4 class="text-center">
+							<?php print $titleItem1 ?>
+						</h4>
+						<h1 class="text-center">
+							<?php print $dataItem1 ?>
+						</h1>
+						<hr>
+						<div class="col-lg-14">
+  							<div class="center-block">
+   		 						<div class="pull-left"><?php print $info1 ?> : <h4 class="center"><?php print $dataInfo1 ?></h4></div>
+								<div class="pull-right"><?php print $info2 ?> : <h4 class="center"><?php print $dataInfo2 ?></h4></div>
 							</div>
-							<a href="#" class="btn btn-primary">GRAPHIQUE</a>
-						</div>
-					</div>
-					<!-- end bloc 1 -->
-
-
-			<!-- bloc 1 -->
-				<div class="grid-1">
-					<div class="card bg-c-blue order-card">
-						<!-- Corps de la carte -->
-						<div class="card-body">
-							<div class="card-block">
-								<h4 class="text-center">
-									<?php print $titleItem2 ?> <!-- Titre de la boxe -->
-								</h4>
-								<h1 class="text-center">
-										<?= print $dataItem2?> €
-									</h1>
-									<hr>
-									<div class="element">
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-										<?php print $info3 ?> :
-										</br><?php print $dataInfo3?>
-										<hr class="vertical">
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-										<?php print $info4 ?>: </hr>
-										<p class="text-right" id="progress"><?php print $dataInfo4?>
-									</div>
-								</div>
 							</div>
-							<a href="#" class="btn btn-primary">GRAPHIQUE</a>
 						</div>
-					</div>
-					<!-- end bloc 1 -->
+					<a href="#" class="btn btn-primary">GRAPHIQUE</a>
+				</div>
+			<!-- end bloc 1 -->
 
-
+				<!-- bloc 1 -->
+				<div class="card bg-c-blue order-card">
+					<!-- Corps de la carte -->
+					<div class="card-body">
+						<!-- <div class="card-block"> -->
+						<h4 class="text-center">
+							<?php print $titleItem2 ?>
+						</h4>
+						<h1 class="text-center">
+							<?php print $dataItem2 ?>
+						</h1>
+						<hr>
+						<div class="col-lg-14">
+  							<div class="center-block">
+   		 						<div class="pull-left"><?php print $info3 ?> : <h4 class="center"><?php print $dataInfo3 ?></h4></div>
+								<div class="pull-right"><?php print $info4 ?> : <h4 class="center"><?php print $dataInfo4 ?></h4></div>
+							</div>
+							</div>
+						</div>
+					<a href="#" class="btn btn-primary">GRAPHIQUE</a>
+				</div>
+			<!-- end bloc 1 -->
--- template/template_boxes4.php
+++ template/template_boxes4.php
@@ -1,136 +1,125 @@
-
-    <head>
-        <meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<link rel="stylesheet" type="text/css" href="tab.css">
-		<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/css/bootstrap-extended.min.css">
-		<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/fonts/simple-line-icons/style.min.css">
-		<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/css/bootstrap.min.css">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
-	</head>
+<head>
+	<meta charset="UTF-4">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="tab.css">
+	<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/css/bootstrap-extended.min.css">
+	<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/fonts/simple-line-icons/style.min.css">
+	<link rel="stylesheet" type="text/css" href="https://pixinvent.com/stack-responsive-bootstrap-4-admin-template/app-assets/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+</head>
 
 <body>
 
 
-<!-- page content -->
+	<!-- page content
 	<section class="page-content">
-		<!-- grid-based layout -->
-			<div class="grid-container-4">
+		rid-based layout -->
+		<div class="grid-container-4">
 
 			<!-- bloc 1 -->
+			<div class="grid-1">
+				<div class="card bg-c-blue order-card">
+					<!-- Corps de la carte -->
+					<div class="card-body">
+						<!-- <div class="card-block"> -->
+						<h4 class="text-center">
+							<?php print $titleItem1 ?>
+						</h4>
+						<h1 class="text-center">
+							<?php print $dataItem1 ?>
+						</h1>
+						<hr>
+						<div class="col-lg-14">
+  							<div class="center-block">
+   		 						<div class="pull-left"><?php print $info1 ?> : <h4 class="center"><?php print $dataInfo1 ?></h4></div>
+								<div class="pull-right"><?php print $info2 ?> : <h4 class="center"><?php print $dataInfo2 ?></h4></div>
+							</div>
+							</div>
+						</div>
+					<a ef="#" class="btn btn-primary">GRAPHIQUE</a>
+				</div>
+			</div>
+			<!-- end bloc 1 -->
+
+			<!-- bloc 1 -->
+			<div class="grid-1">
+				<div class="card bg-c-blue order-card">
+					<!-- Corps de la carte -->
+					<div class="card-body">
+						<!-- <div class="card-block"> -->
+						<h4 class="text-center">
+							<?php print $titleItem2 ?>
+						</h4>
+						<h1 class="text-center">
+							<?php print $dataItem2 ?>
+						</h1>
+						<hr>
+						<div class="col-lg-14">
+  							<div class="center-block">
+   		 						<div class="pull-left"><?php print $info3 ?> : <h4 class="center"><?php print $dataInfo3 ?></h4></div>
+								<div class="pull-right"><?php print $info4 ?> : <h4 class="center"><?php print $dataInfo4 ?></h4></div>
+							</div>
+							</div>
+						</div>
+					<a ef="#" class="btn btn-primary">GRAPHIQUE</a>
+				</div>
+			</div>
+			<!-- end bloc 1 -->
+
+				<!-- bloc 1 -->
 				<div class="grid-1">
-					<div class="card bg-c-blue order-card">
-						<!-- Corps de la carte -->
-						<div class="card-body">
-							<div class="card-block">
-								<h4 class="text-center">
-									<?php print $titleItem1 ?> <!-- Titre de la boxe -->
-								</h4>
-									<h1 class="text-center">
-											<?= print $dataItem1?> €
-									</h1>
-									<hr>
-									<div class="element"></br>
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-										<?php print $info1 ?> :
-										</br><?php print $dataInfo1?>
-										<hr class="vertical">
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-										<?php print $info2 ?> :
-										<p class="text-right" id="progress"><?php print $dataInfo2?></p>
-									</div>
-								</div>
+				<div class="card bg-c-blue order-card">
+					<!-- Corps de la carte -->
+					<div class="card-body">
+						<!-- <div class="card-block"> -->
+						<h4 class="text-center">
+							<?php print $titleItem3 ?>
+						</h4>
+						<h1 class="text-center">
+							<?php print $dataItem3 ?>
+						</h1>
+						<hr>
+						<div class="col-lg-14">
+  							<div class="center-block">
+   		 						<div class="pull-left"><?php print $info5 ?> : <h4 class="center"><?php print $dataInfo5 ?></h4></div>
+								<div class="pull-right"><?php print $info6 ?> : <h4 class="center"><?php print $dataInfo6 ?></h4></div>
 							</div>
-							<a href="#" class="btn btn-primary">GRAPHIQUE</a>
-						</div>
-					</div>
-					<!-- end bloc 1 -->
-
-
-					<!-- bloc 2 -->
-					<div class="grid-2">
-					<div class="card bg-c-blue order-card">
-						<!-- Corps de la carte -->
-						<div class="card-body">
-							<div class="card-block">
-								<h4 class="text-center">
-									<?php print $titleItem2 ?>
-								</h4>
-								<h1 class="text-center">
-											<?= print $dataItem2?> €
-									</h1>
-									<hr>
-									<div class="element">
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-										<?php print $info3 ?> :
-										<p class="text-right"><?php print $dataInfo3?>
-										<hr class="vertical">
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-										<?php print $info4 ?> :
-										<?php print $dataInfo4?>
-									</div>
-								</div>
 							</div>
-							<a href="#" class="btn btn-primary">GRAPHIQUE</a>
 						</div>
-					</div>
-					<!-- end bloc 2 -->
+					<a href="#" class="btn btn-primary">GRAPHIQUE</a>
+				</div>
+			</div>
+			<!-- end bloc 1 -->
 
-						<!-- bloc 3 -->
-						<div class="grid-3">
-						<div class="card bg-c-blue order-card">
-							<!-- Corps de la carte -->
-							<div class="card-body">
-								<div class="card-block">
-									<h4 class="text-center">
 
-										<?php print $titleItem3 ?>
-									</h4>
-									<h1 class="text-center">
-											<?= print $dataItem3?> €
-									</h1>
-									<hr>
-									<div class="element">
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-											<?php print $info5 ?> :
-											</br><?php print $dataInfo5?>
-										<hr class="vertical">
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-											<?php print $info6 ?> :
-											<?php print $dataInfo6?> € </p>
-									</div>
-								</div>
+				<!-- bloc 1 -->
+				<div class="grid-1">
+				<div class="card bg-c-blue order-card">
+					<!-- Corps de la carte -->
+					<div class="card-body">
+						<!-- <div class="card-block"> -->
+						<h4 class="text-center">
+							<?php print $titleItem4 ?>
+						</h4>
+						<h1 class="text-center">
+							<?php print $dataItem4 ?>
+						</h1>
+						<hr>
+						<div class="col-lg-14">
+  							<div class="center-block">
+   		 						<div class="pull-left"><?php print $info7 ?> : <h4 class="center"><?php print $dataInfo7 ?></h4></div>
+								<div class="pull-right"><?php print $info8 ?> : <h4 class="center"><?php print $dataInfo8 ?></h4></div>
 							</div>
-							<a href="#" class="btn btn-primary">GRAPHIQUE</a>
-						</div>
-					</div>
-						<!-- end bloc 3 -->
-
-						<div class="grid-3">
-						<div class="card bg-c-blue order-card">
-							<!-- Corps de la carte -->
-							<div class="card-body">
-								<div class="card-block">
-									<h4 class="text-center">
-										<?php print $titleItem4 ?>
-									</h4>
-									<h1 class="text-center">
-											<?= print $dataItem4?> €
-									</h1>
-									<hr>
-									<div class="element">
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-											<?php print $info7 ?> :
-											</br><?php print $dataInfo7?>
-										<hr class="vertical">
-										<p class="text-right"></p> <!-- Donnée par rapport à l'année ou au mois dernier -->
-											<?php print $info8 ?> :
-											<?php print $dataInfo8?> € </p>
-									</div>
-								</div>
 							</div>
-							<a href="#" class="btn btn-primary">GRAPHIQUE</a>
 						</div>
-					</div>
-						<!-- end bloc 3 -->
+					<a href="#" class="btn btn-primary">GRAPHIQUE</a>
+				</div>
+			</div>
+			<!-- end bloc 1 -->
+
+
+
+
+
+
 
