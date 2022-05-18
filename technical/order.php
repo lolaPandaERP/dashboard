636f4f6e79c95702f704f9e6816420d3d3bfc4ab
lolaPandaERP <86048238+lolaPandaERP@users.noreply.github.com>
2022-05-18 11:27:22 +0200

    Update order.php

--- technical/order.php
+++ technical/order.php
@@ -68,14 +68,161 @@ $formfile = new FormFile($db);
 $general = new General($db);
 $object = new Technique($db);
 
-// Template NavBar
-$head = '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">';
 llxHeader($head, $langs->trans("Commande"));
 
 print load_fiche_titre($langs->trans("Commande"));
 
 // Chargement du template de navigation pour l'activité "Global"
 print $object->load_navbar();
+
+$titleItem1 = "Commandes en cours";
+$titleItem2 = "Commandes en cours";
+$titleItem3 = "Commandes clients en cours";
+
+include DOL_DOCUMENT_ROOT.'/custom/tab/template/template_boxes2.php';
+
+$titleItem4 = "Commandes clients en cours";
+$titleItem5 = "Commandes les + importantes";
+$titleItem6 = "Commandes les + anciennes";
+
+?>
+
+<!-- DATE ET CLIENT LA + PROCHE  -->
+<div class="grid-list">
+	<div class="card bg-c-blue order-card">
+		<!-- Corps de la carte -->
+		<div class="card-body">
+			<div class="card-block">
+				<h4 class="text-center">
+					<?php print $titleItem4 ?>
+				</h4>
+				<hr>
+				<p class="text-center">
+					<?php
+
+					// $result3 = $object->fetchOrderSortedByDeliveryDate($firstDayYear, $lastDayYear);
+
+					// if(is_array($result3) && $result3 != null){
+
+					// 	foreach ($result3 as $res)
+					// 	{
+					// 		$societe = new Societe($db);
+					// 		$societe->fetch($res->fk_soc);
+
+					// 		$commande = new Commande($db);
+					// 		$commande->fetch($res->rowid);
+
+					// 		print '<ul class="list-group">';
+					// 		print '<li class="list-group-item d-flex justify-content-between align-items-center">';
+					// 		print  '<i class="fas fa-address-card fa-2x"></i>'.$societe->name;
+
+					// 		if($commande->date_livraison != null) {
+					// 			print '<span class="badge-* badge-pill badge-primary">Date de livraison prévue : '.date('j-m-Y', $commande->date_livraison).'</span></li>';
+					// 		} else {
+					// 			print '<span class="badge-* badge-pill badge-warning">Aucune date de livraison spécifiée</span></li>';
+					// 		}
+					// 		print '</ul>';
+					// 	}
+
+					// }
+
+					?>
+					</p>
+				 </div>
+			</div>
+		</div>
+	</div>
+	<!-- BOXE WITHOUT STATS -->
+
+	<!-- PRODUCTION LES + PROCHES -->
+<div class="grid-list">
+	<div class="card bg-c-blue order-card">
+		<!-- Corps de la carte -->
+		<div class="card-body">
+			<div class="card-block">
+				<h4 class="text-center">
+					<?php print $titleItem5 ?>
+				</h4>
+				<hr>
+				<p class="text-center">
+					<?php
+
+					//  $result = $object->fetchDeliveredOrderToday();
+
+					// if($result != null){
+
+					// 	foreach ($result as $res)
+					// 	{
+					// 		$societe = new Societe($db);
+					// 		$societe->fetch($res->fk_soc);
+
+					// 		$commande = new Commande($db);
+					// 		$commande->fetch($res->rowid);
+
+					// 		print '<ul class="list-group">';
+					// 		print '<li class="list-group-item d-flex justify-content-between align-items-center">';
+					// 		print  '<i class="fas fa-address-card fa-2x"></i>'.$societe->name;
+
+					// 		if($commande->date_livraison != null) {
+					// 			print '<span class="badge-* badge-pill badge-primary">Date de création : '.date('j-m-Y', $commande->date_commande).'</span></li>';
+					// 		} else {
+					// 			print '<span class="badge-* badge-pill badge-warning">Aucune date de livraison spécifiée</span></li>';
+					// 		}
+					// 		print '</ul>';
+					// 	}
+					// } else {
+					// 	print '<p class="center"><pan class="badge-* badge-pill badge-danger">Aucune commande validées pour ce jour </spa</p>';
+					// }
+
+					?>
+					</p>
+				 </div>
+			</div>
+		</div>
+	</div>
+
+
+<!-- CUSTOMER TO BE PRODUCED  -->
+<div class="grid-list">
+	<div class="card bg-c-blue order-card">
+		<!-- Corps de la carte -->
+		<div class="card-body">
+			<div class="card-block">
+				<h4 class="text-center">
+					<?php print $titleItem6 ?>
+				</h4>
+				<hr>
+				<p class="text-center">
+					<?php
+
+					// $rets = $object->fetchOrder(1);
+
+					// if(is_array($rets) && $rets != null){
+
+					// 	foreach ($rets as $ret)
+					// 	{
+					// 		$societe = new Societe($db);
+					// 		$societe->fetch($ret->fk_soc);
+
+					// 		$commande = new Commande($db);
+					// 		$commande->fetch($ret->rowid);
+
+					// 		print '<ul class="list-group">';
+					// 		print '<li class="list-group-item d-flex justify-content-between align-items-center">';
+					// 		print  '<i class="fas fa-address-card fa-2x"></i>'.$societe->name; // redirection vers module tiers - onglet client
+					// 		print '<span class="badge-* badge-pill badge-primary">Réf. commande :  '.$commande->ref.'</span></li>';
+					// 		print '</ul>';
+					// 	}
+					// }
+
+					?>
+					</p>
+				 </div>
+			</div>
+		</div>
+	</div>
+
+	<?php
 
 // End of page
 llxFooter();
